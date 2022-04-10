0bc369e2178fc85066225f44c713d4af6cbdfdca
ChiyukiOfficial <ChiyukiOfficial@users.noreply.github.com>
2022-04-10 13:43:29 +0000

    deploy: f50c39ba9db922862b96203c92540b8ae887d021

---
+++ js/loaded.js
@@ -0,0 +1,12 @@
+﻿window.onload = () => {
+    const loader = document.getElementById('loader');
+    sleep(3000);
+    loader.classList.add('loaded');
+}
+
+function sleep(waitMsec) {
+    var startMsec = new Date();
+
+    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+    while (new Date() - startMsec < waitMsec);
+}
